ac5cb414070ed96af68772040e6cd49666705779
BreezeWhite <freedombluewater@gmail.com>
2020-07-09 15:59:08 +0800

    Update

--- scripts/evaluate_from_pred.sh
+++ scripts/evaluate_from_pred.sh
@@ -4,9 +4,27 @@ MODEL_NAME=feature-compare_harmonic
 PRED_FOLDER=val_feature-compare_harmonic
 TH=0.3
 
+# ----------------------------- MusicNet -----------------------------------
+# Attn-LS
+MODEL_NAME=MusicNet-Attn-Note-Smooth-V1.0.2
+PRED_FOLDER=musicnet_smooth_v1.0.2
+TH=0.045
+
+# Conv
+#MODEL_NAME=ICASSP-2019-MusicNet-Note
+#PRED_FOLDER=icassp_2019_musicnet_note
+#TH=0.05
+
+#Conv-LS
+#MODEL_NAME=Dilated-Conv-MusicNet-Note-Smooth
+#PRED_FOLDER=dilated_conv_musicnet_note_smooth
+#TH=0.04
+
+# --------------------------------------------------------------------------
+
 MODE=mpe_note
-PRED_PATH="./prediction-paper/${PRED_FOLDER}/${MODEL_NAME}_predictions.hdf"
-LABEL_PATH="./prediction-paper/${PRED_FOLDER}/${MODEL_NAME}_labels.pickle"
+PRED_PATH="./prediction/${PRED_FOLDER}/${MODEL_NAME}_predictions.hdf"
+LABEL_PATH="./prediction/${PRED_FOLDER}/${MODEL_NAME}_labels.pickle"
 
 #PRED_PATH="./prediction/${PRED_FOLDER}/pred.hdf"
 #LABEL_PATH="./prediction/${PRED_FOLDER}/${MODEL_NAME}_labels.pickle"
