265f2d27f9ab16429b5b0347367a7185a44737ea
BreezeWhite <freedombluewater@gmail.com>
2020-07-09 15:21:16 +0800

    General update of train model

--- scripts/train_model.sh
+++ scripts/train_model.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-out_model=full_model_all_feature_musicnet
+out_model=feature-compare_harmonic
 epoch=15
 early_stop=6
+#label_type=multi_instrument_note
+label_type=frame_onset
 
 cd ..
-python3 TrainModel.py MusicNet $out_model \
+python3 TrainModel.py Maestro $out_model \
     --channels 1 2 3         \
+    --label-type $label_type \
     --epoch $epoch           \
     --steps 3000             \
     --timesteps 128          \
     --early-stop $early_stop \
     --train-batch-size 8     \
-    --val-batch-size 8       
-    #--use-harmonic
+    --val-batch-size 8       \
+    --use-harmonic
     #-i ./model/Maestro-Smooth-Ultimate-Attn-V1
 
